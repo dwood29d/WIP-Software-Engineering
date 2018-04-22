8da4625b5aa6c9e962dbb52da04e4c827a628896
Devin Wood <dwood29d@gmail.com>
2018-04-21 19:52:06 -0500

    updatetickets.php now verifies that the new number of seats is greater than the number of tickets already purchased.

--- MoviesPro/updatetickets.php
+++ MoviesPro/updatetickets.php
@@ -9,6 +9,8 @@ $q = "SELECT * FROM theaters";
 $r = @mysqli_query($dbc, $q); // Run the query
 $q2 = "SELECT * FROM movies";
 $r2 = @mysqli_query($dbc, $q2); // Run the query
+$query_num_seats = "SELECT seats_purchased FROM movies_theaters WHERE movie_id = $mo AND theater_id = $th";
+$res = @mysqli_query($dbc, $query_num_seats);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $errors = array();
@@ -31,20 +33,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   if (empty($_POST['new_total_seats'])) {
     $errors[] = 'You forgot to specify the new number of seats for this theater.';
   } else {
-    $nts = mysqli_real_escape_string($dbc, trim($_POST['num_tickets']));
+    $nts = mysqli_real_escape_string($dbc, trim($_POST['new_total_seats']));
   } //echo "$th";
 
   if (empty($_POST['new_price'])) {
     $errors[] = 'You forgot to specify the new price for this theater.';
   } else {
-    $np = mysqli_real_escape_string($dbc, trim($_POST['email']));
+    $np = mysqli_real_escape_string($dbc, trim($_POST['new_price']));
   }
+
+  if ($res > $_POST['new_total_seats']) {
+    $errors[] = 'The number of purchased tickets is already greater than the new number of available seats.';
+  }
+
 
   if (empty($errors)) { // Postback and all clear
 
     // Make an update query'
     for ($i = 0; $i < $_POST['num_tickets']; $i++){
-      $q3 = "UPDATE theaters SET seats_per_theater = $nts, price = $np WHERE theater_id = $th"; //"INSERT INTO tickets (movie_id, theater_id, price) VALUES ($mo, $th, 0.00)";
+
+      $q3 = "UPDATE theaters SET seats_per_theater = $nts, price = $np WHERE theater_id = $th";
       $r3 = @mysqli_query($dbc, $q3); // Create a ticket record
     }
     if ($r3) { // If the query worked.
